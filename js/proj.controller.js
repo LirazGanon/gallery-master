019917dcb1824fe9b4a58def304f112a7dbd0539
לירז גנון <lirazganon@gmail.com>
2022-10-14 15:02:56 +0300

    test

--- js/proj.controller.js
+++ js/proj.controller.js
@@ -6,20 +6,20 @@ function renderProjs() {
     if (!projs || !projs.length) return
 
     var strHtmls = projs.map(proj => `
-    <article data-id="${proj.id}" class="col-md-4 col-sm-6 portfolio-item">
+    <article data-id="${proj.id}" class="col-md-4 col-sm-6 portfolio-item p-0  m-3">
     <a class="portfolio-link" data-toggle="modal" href="#portfolioModal">
     <div class="portfolio-hover">
-    <div class="portfolio-hover-content">
-    <i class="fa fa-plus fa-3x"></i>
-    </div>
-    </div>
-    <img class="img-fluid" src="img/portfolio/${proj.id}.png" >
-    </a>
-    <div class="portfolio-caption">
-    <h4>${proj.name}</h4>
-    <p class="text-muted">${proj.title}</p>
-    </div>
-   </article>
+          <div class="portfolio-hover-content">
+            <i class="fa fa-plus fa-3x"></i>
+          </div>
+        </div>
+        <img class="img-fluid" src="img/portfolio/${proj.id}.png">
+      </a>
+      <div class="portfolio-caption">
+        <h4>${proj.name}</h4>
+        <p class="text-muted">${proj.title}</p>
+      </div>
+  </article>
         `
     )
     $('.proj-container').html(strHtmls)
