66f1d92b5847f99719c991f483e830bfdd20335e
לירז גנון <lirazganon@gmail.com>
2022-10-15 19:46:06 +0300

    118n add beta

--- js/proj.controller.js
+++ js/proj.controller.js
@@ -2,10 +2,10 @@
 renderProjs()
 
 function renderProjs() {
-    var projs = getProjs()
-    if (!projs || !projs.length) return
+  var projs = getProjs()
+  if (!projs || !projs.length) return
 
-    var strHtmls = projs.map(proj => `
+  var strHtmls = projs.map(proj => `
     <article data-id="${proj.id}" class="col-md-4 col-sm-6 portfolio-item p-0  m-3">
     <a class="portfolio-link" data-toggle="modal" href="#portfolioModal">
     <div class="portfolio-hover">
@@ -17,44 +17,65 @@ function renderProjs() {
       </a>
       <div class="portfolio-caption">
         <h4>${proj.name}</h4>
-        <p class="text-muted">${proj.title}</p>
+        <p class="text-muted" data-trans="${proj.id}-title">${proj.title}</p>
       </div>
   </article>
         `
-    )
-    $('.proj-container').html(strHtmls)
-    $('.portfolio-item').on('click', onRenderModal)
-    $('.year').text(new Date().getFullYear())
+  )
+  $('.proj-container').html(strHtmls)
+  $('.portfolio-item').on('click', onRenderModal)
+  $('.year').text(new Date().getFullYear())
+  $('body').on('click', closeCanvas)
+
 }
 
 function onSendMail(ev) {
-    ev.preventDefault()
-    const name = ev.target.name.value
-    const email = ev.target.email.value
-    const subject = ev.target.subject.value
-    const body = ev.target.message.value
-    window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=lirazganon@gmail.com&su=${subject}&body=${body}`,"blank")
-    openCanvas()  
+  ev.preventDefault()
+  const name = ev.target.name.value
+  const email = ev.target.email.value
+  const subject = ev.target.subject.value
+  const body = ev.target.message.value
+  window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=lirazganon@gmail.com&su=${subject}&body=${body}`, "blank")
+  openCanvas()
 }
 
 
 
 function onRenderModal(projId) {
-    var projId = $(this).data().id
-    console.log(projId)
-    var proj = getProjs().find((proj) => proj.id === projId)
+  var projId = $(this).data().id
+  console.log(projId)
+  var proj = getProjs().find((proj) => proj.id === projId)
 
-    const $modal = $('.project-details')
-    $modal.children('h2').text(proj.name)
-    $modal.children('.item-intro').text(proj.title)
-    $modal.children('.tags').text(proj.labels.join(" "))
-    $modal.children('.img-fluid').attr('src', `img/portfolio/${proj.id}-full.png`)
-    $modal.children('.project-link').attr('href', proj.url)
-    $modal.children('.desc').text(proj.desc)
-    $modal.find('.proj-date').text(proj.publishedAt)
-    $modal.find('.proj-category').text(proj.category)
-
+  const $modal = $('.project-details')
+  $modal.children('h2').text(proj.name)
+  $modal.children('.item-intro').text(proj.title)
+  $modal.children('.item-intro').attr('data-trans', `${proj.id}-title`)
+  $modal.children('.tags').text(proj.labels.join(" "))
+  $modal.children('.img-fluid').attr('src', `img/portfolio/${proj.id}-full.png`)
+  $modal.children('.project-link').attr('href', proj.url)
+  $modal.children('.desc').text(proj.desc)
+  $modal.find('.proj-date').text(proj.publishedAt)
+  $modal.find('.proj-category').text(proj.category)
+  // doTrans()
 }
 
+function onSetLang() {
+  setLang()
+  setDirection(gCurrLang)
+  doTrans()
+}
 
+function setDirection(lang) {
+  if (lang === 'he') {
+    document.body.classList.add('rtl')
+    $('.navbar-nav').addClass('navbar-nav-he')
+    $('.lang-flag').attr('src', 'img/GB.png')
+    $('.lang-flag').attr('title', 'Switch Language')
+  } else{
+    document.body.classList.remove('rtl')
+    $('.navbar-nav').removeClass('navbar-nav-he')
+    $('.lang-flag').attr('src', 'img/IL.png')
+    $('.lang-flag').attr('title', 'שנה שפה')
+  } 
+}
 
---
+++ js/service/i18n.service.js
@@ -0,0 +1,111 @@
+
+var gTrans = {
+    'header-secondary-title': {
+        en: 'Welcome To my Portfolio',
+        he: 'ברוכים הבאים לפורטפוליו שלי'
+    },
+    'header-main-title': {
+        en: 'It\'s Nice To Meet You',
+        he: 'נעים להכיר אותך!'
+    },
+    'tell-me-more': {
+        en: 'Tell Me More',
+        he: 'ספר לי עוד'
+    },
+    'specialities': {
+        en: 'Specialities',
+        he: 'מיומנויות'
+    },
+    'specialities-desc': {
+        en: 'Lorem ipsum dolor sit amet consectetur.',
+        he: 'אז מה אני יודע לעשות?'
+    },
+    'javascript-desc': {
+        en: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architectoquo inventore harum exmagni, dicta impedit.',
+        he: 'ג\'אווה סקריפט ואוו איזה יופי זה מדהים ואוו ואתרים והכל ופוקי'
+    },
+    'react-desc': {
+        en: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architectoquo inventore harum exmagni, dicta impedit.',
+        he: 'ריאקט ואוו איזה יופי זה מדהים ואוו ואתרים והכל ופוקי'
+    },
+    'vue-desc': {
+        en: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architectoquo inventore harum exmagni, dicta impedit.',
+        he: 'ויו ואוו איזה יופי זה מדהים ואוו ואתרים והכל ופוקי'
+    },
+    'portfolio': {
+        en: 'Portfolio',
+        he: 'תיק עבודות'
+    },
+    'portfolio-desc': {
+        en: 'Lorem ipsum dolor sit amet consectetur.',
+        he: 'מוזמנים להיכנס ולהתרשם מהפרוייקטים שלי'
+    },
+    'portfolio-desc': {
+        en: 'Lorem ipsum dolor sit amet consectetur.',
+        he: 'מוזמנים להיכנס ולהתרשם מהפרוייקטים שלי'
+    },
+    'touchNUM-title': {
+        en: 'Lorem ipsum dolor sit amet consectetur.',
+        he: 'מוזמנים להיכנס ולהתרשם מהפרוייקטים שלי'
+    },
+    'about-me': {
+        en: `Hi, I am Liraz!
+        I own a Hummus place, really love to code and solve problems.
+        Contact me if you are hungry :)`,
+        he: `היי אני לירז!
+        אני בעלים של חומוסייה, מאוד אוהב קוד ולפתור בעיות.
+         צרו איתי קשר אם אתם רעבים :)`
+    },
+    'me': {
+        en: 'Me',
+        he: 'הכירו אותי'
+    },
+    'contact': {
+        en: 'Contact',
+        he: 'צרו קשר'
+    },
+}
+
+
+let gCurrLang = 'en'
+
+var gTransProj = getTransProj()
+gTrans = { ...gTrans, ...gTransProj }
+console.log(gTrans)
+
+function getTransProj() {
+    const transProj = {}
+    gProjects.forEach((proj) => {
+        transProj[`${proj.id}-title`] = {
+            en: proj.title,
+            he: proj.titleHe
+        }
+    })
+
+    return transProj
+}
+
+function getTrans(transKey) {
+    const transMap = gTrans[transKey]
+    if (!transMap) return 'UNKNOWN'
+
+    let trans = transMap[gCurrLang]
+    if (!trans) trans = transMap.en
+    return trans
+}
+
+function doTrans() {
+    const els = document.querySelectorAll('[data-trans]')
+    els.forEach(el => {
+        const transKey = el.dataset.trans
+        const trans = getTrans(transKey)
+        el.innerText = trans
+        if (el.placeholder) el.placeholder = trans
+    })
+
+}
+
+function setLang() {
+    if (gCurrLang === 'en') gCurrLang = 'he'
+    else gCurrLang = 'en'
+}
